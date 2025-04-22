eb39d2f1d74aab3aaa89cc31f63de9f750293e4c
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-04-22 16:53:09 +0530

    changes

--- 4-Laravel Blade Template - I/resources/views/welcome.blade.php
+++ 4-Laravel Blade Template - I/resources/views/welcome.blade.php
@@ -1,8 +1,62 @@
 <h1>
     Welcome to Laravel 11
-
 </h1>
 
-<br>
+{{ 5 * 5 }}
+{{ 5 + 5 }}
+{{ 5 - 5 }}
+{{ 5 / 5 }}
+{{ 5 % 2 }}
+{{ 5 ** 2 }}
+{{ 5 == 5 }}
+{{ 5 === 5 }}
+{{ 5 != 5 }}
+{{ 5 !== 5 }}
+{{ 5 > 5 }}
+{{ 5 < 5 }}
+{{ 5 >= 5 }}
+{{ 5 <= 5 }}
 
-<a href="{{ route('about', ['id' => 1]) }}">About</a>
+{{ "Hello World" }}
+
+{{--  {!!"<script>alert('dfgdf')</script>"!!}  --}}
+
+<br>
+@php
+    $name = "John Doe";
+    $age = 30;
+    $isAdmin = true;
+    $user = [
+        'name' => 'John Doe',
+        'age' => 30,
+        'isAdmin' => true,
+    ];
+@endphp
+{{ $name }}
+{{ $age }}
+{{ $isAdmin }}
+{{ $user['name'] }}
+{{ $user['age'] }}
+{{ $user['isAdmin'] }}
+
+@php
+    $array = [1, 2, 3, 4, 5];
+    $user="John Doe";
+@endphp
+<br><br>
+<ul>
+@foreach ($array as $item)
+    {{--  <li>{{ $loop->index }}:{{ $item }}</li>  --}}
+    @if($loop->first)
+        <li>First:{{ $item }}</li>
+    @elseif($loop->last)
+        <li>Last:{{ $item }}</li>
+    @else
+        <li>Middle:{{ $item }}</li>
+    @endif
+
+@endforeach
+</ul>
+<br><br>
+@{{ $user }}
+
--- 4-Laravel Blade Template - I/routes/web.php
+++ 4-Laravel Blade Template - I/routes/web.php
@@ -2,34 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return "hello world";
-});
-Route::view('/', 'welcome');
-
-// Route::get('/about/{id?}', function (string $id=null) {
-//     return view('about');
-// })->name('about');
-
-
-Route::prefix('page')->group(function () {
-    Route::get('/about', function () {
-        return "about";
-    })->name('about');
-    Route::get('/contact', function () {
-        return "contact";
-    })->name('contact');
-    Route::get('/services', function () {
-        return "services";
-    })->name('services');
-
-});
-
-
-Route::fallback(function () {
-    return "404";
+    return view('welcome');
 });
