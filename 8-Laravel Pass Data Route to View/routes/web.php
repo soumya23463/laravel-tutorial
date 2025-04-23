ec2883bf99addb8841ee6d9adbb7df709e6c8183
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-04-23 13:21:21 +0530

    changes

--- 8-Laravel Pass Data Route to View/routes/web.php
+++ 8-Laravel Pass Data Route to View/routes/web.php
@@ -2,17 +2,93 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     $title = 'Welcome to My Website';
+//     return view('welcome', [
+//         'title' =>  $title,
+//         'description' => 'This is a sample description for the home page.',
+//         'keywords' => 'home, welcome, sample',
+//         // 'script' => '<script>alert("Welcome to the home page!");</script>',
+//     ]);
+// })->name('home');
+
+
+// Route::get('/', function () {
+//     $title = 'Welcome to My Website';
+//     return view('welcome' )->with('title',$title);
+// })->name('home');
+
+
+// Route::get('/', function () {
+//     $title = 'Welcome to My Website';
+//     return view('welcome' )->withTitle($title);
+// })->name('home');
+
+
+// Route::get('/', function () {
+//    $users = [
+//         1=>[
+//             'name' => 'John',
+//             'email' => 'john@example.com',
+//             'age' => 30
+//         ],
+//         2=>[
+//             'name' => 'Alice',
+//             'email' => 'alice@example.com',
+//             'age' => 25
+//         ],
+//         3=>[
+//             'name' => 'Bob',
+//             'email' => 'bob@example.com',
+//             'age' => 28
+//         ]
+//     ];
+//     return view('welcome' )->withUser($users);
+// })->name('home');
+
+
 Route::get('/', function () {
-    return view('welcome');
+    $users =[
+        1 => [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'age' => 30
+        ],
+        2 => [
+            'name' => 'Alice',
+            'email' => 'alice@example.com',
+            'age' => 25
+        ],
+        3 => [
+            'name' => 'Bob',
+            'email' => 'bob@example.com',
+            'age' => 28
+        ]
+    ];
+    return view('welcome')->withUser($users);
 })->name('home');
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
 
-Route::get('/post', function () {
-    return view('post');
-})->name('post');
-
-Route::get('/test',function(){
-    return view('test');
-});
+Route::get('/user/{id}', function ($id) {
+    $users = [
+        1 => [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'age' => 30
+        ],
+        2 => [
+            'name' => 'Alice',
+            'email' => 'alice@example.com',
+            'age' => 25
+        ],
+        3 => [
+            'name' => 'Bob',
+            'email' => 'bob@example.com',
+            'age' => 28
+        ]
+    ];
+    $user = $users[$id] ?? null;
+    abort_if(!$user, 404, 'User not found');
+    return view('user', [
+        'id' => $user
+    ]);
+})->name('view.user');
