76682853801755d77913cf4f95ffee5cebf967b6
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-04-24 11:32:48 +0530

    changes

--- 15-Laravel Eloquent ORM/routes/web.php
+++ 15-Laravel Eloquent ORM/routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\UserController;
 
-// Route::get('/', [PageController::class, 'home'])->name('home');
-// Route::get('/user/{id}', [PageController::class, 'index'])->name('user');
-
-
-Route::controller(PageController::class)->group(function () {
-    Route::get('/', 'home')->name('home');
-    Route::get('/user/{id}', 'index')->name('user');
-});
-Route::get('/test',PageController::class)->name('test');
+Route::get('/', [UserController::class, 'index'])->name('home');
